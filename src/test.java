2616f134fdefdf748165b93f7da6758ab9091c57
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-02 17:46:22 -0700

    test.java

--- src/test.java
+++ src/test.java
@@ -1,10 +1,9 @@
 public class test{
   public static void main(String[] args){
-	  String port = "";
+	  for(int i = 0; i > 5; i +=6){
+		  System.out.println(i);
+		}
 
-	  for(int i = 0; i < 3; i++){
-	    port = port + i;
-	  }
 
   }
 }
